9e578bc78d87631a4826d863c95095bea599bd42
ThanhThuyHuynh <20130429@st.hcmuaf.edu.vn>
2022-10-28 23:20:41 +0700

    add back to top button and change title

--- js/main.js
+++ js/main.js
@@ -222,3 +222,16 @@
     });
 
 })(jQuery);
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
+$('.back-to-top').click(function () {
+    $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    return false;
+});
