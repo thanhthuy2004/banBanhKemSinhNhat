0a4b804b5695c28864a435c0e2784c9866ee1e88
ThanhThuyHuynh <20130429@st.hcmuaf.edu.vn>
2022-11-07 13:54:07 +0700

    fix shop-product.html, delete 1 filter item

--- js/main.js
+++ js/main.js
@@ -181,12 +181,12 @@ function myFunction() {
         max: maxPrice,
         values: [minPrice, maxPrice],
         slide: function (event, ui) {
-            minamount.val('$' + ui.values[0]);
-            maxamount.val('$' + ui.values[1]);
+            minamount.val(ui.values[0]);
+            maxamount.val(ui.values[1]);
         }
     });
-    minamount.val('$' + rangeSlider.slider("values", 0));
-    maxamount.val('$' + rangeSlider.slider("values", 1));
+    minamount.val(rangeSlider.slider("values", 0));
+    maxamount.val(rangeSlider.slider("values", 1));
 
     /*--------------------------
         Select
