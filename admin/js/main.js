5446422315d092378e5399e0d59bb16d5b788bb1
ThanhThuyHuynh <20130429@st.hcmuaf.edu.vn>
2022-11-12 17:00:07 +0700

    change js back to top button

--- admin/js/main.js
+++ admin/js/main.js
@@ -21,7 +21,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 80, 'easeInOutExpo');
         return false;
     });
 
--- js/main.js
+++ js/main.js
@@ -243,7 +243,7 @@ function myFunction() {
     }
 });
 $('.back-to-top').click(function () {
-    $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    $('html, body').animate({scrollTop: 0}, 1000, 'easeInOutExpo');
     return false;
 });
 
